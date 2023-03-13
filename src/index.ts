87aff8235facd813d0d686921be52a46417f3b12
icepick4 <remi.jara@laposte.net>
2023-03-13 20:23:14 +0100

    api working, keeping this architecture for the moment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import express from 'express';
+import countryRouter from './routes/countryRouter';
 import userRouter from './routes/userRouter';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/users', userRouter);
+app.use('/countries', countryRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import * as userModel from '../models/user.model';
-import { User } from '../types/user';
+import { User, UserScore } from '../types/user';
 
 const userRouter = express.Router();
 
@@ -23,9 +23,40 @@ userRouter.post('/', async (req: Request, res: Response) => {
     });
 });
 
-userRouter.put('/:id', async (req: Request, res: Response) => {
+userRouter.post('/score/', async (req: Request, res: Response) => {
+    const userScore: UserScore = req.body;
+    userModel.createScore(userScore, (err: Error, userId: number) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(200).json({ userId: userId });
+    });
+});
+
+userRouter.put('/', async (req: Request, res: Response) => {
     const user: User = req.body;
     userModel.update(user, (err: Error) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(200).send();
+    });
+});
+
+userRouter.put('/score/', async (req: Request, res: Response) => {
+    const userScore: UserScore = req.body;
+
+    userModel.updateScore(userScore, (err: Error) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(200).send();
+    });
+});
+
+userRouter.delete('/:id', async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id);
+    userModel.remove(id, (err: Error) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
